d9085e4e9407220a644dfb1ef041838522f56d32
bingz27 <bingz27@gmail.com>
2014-06-10 11:20:11 -0400

    Update Image Path Processing

--- smallSampleRead/plotROIAvgPixels.m
+++ smallSampleRead/plotROIAvgPixels.m
@@ -3,17 +3,18 @@ clear
 
 %%% User Inputs
 roiStart1 = [745, 590]; %top left point of roi [x,y]
-roiStart2 = [760, 590];
+%roiStart2 = [760, 590];
 roiSideLength = 15;
 nFrame = 2000;
 BaseFilename = Filename('img_', .000000001,'_Default_000.tif'); %based on camera image naming system
+imageDirectory = 'C:\Users\Loturco\Desktop\Bing - Matlab Analysis\sample data\5msecglutamateprepost_1';
 %%% User Inputs
 
 roiRegion1 = setroi(roiStart1, roiSideLength);
-roiRegion2 = setroi(roiStart2, roiSideLength);
-storedActive = parseimages('C:\Users\Loturco\Desktop\Bing - Matlab Analysis\sample data\5msecglutamateprepost_1', BaseFilename, roiRegion1, nFrame);
-storedInactive = parseimages('C:\Users\Loturco\Desktop\Bing - Matlab Analysis\sample data\5msecglutamateprepost_1', BaseFilename, roiRegion2, nFrame, +185);
-markroi('C:\Users\Loturco\Desktop\Bing - Matlab Analysis\sample data\5msecglutamateprepost_1', BaseFilename, roiStart1, roiSideLength, roiStart2, roiSideLength);
+%roiRegion2 = setroi(roiStart2, roiSideLength);
+storedActive = parseimages(imageDirectory, BaseFilename, roiRegion1, nFrame);
+%storedInactive = parseimages(imageDirectory, BaseFilename, roiRegion2, nFrame, +185);
+markroi(imageDirectory, BaseFilename, roiStart1, roiSideLength);
 
 fprintf('Plotting Data...\n')
 time = 1:nFrame;
