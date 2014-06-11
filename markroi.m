78e43f18eebe69cdb507cc95995f2c7b1c3b0383
bingz27 <bingz27@gmail.com>
2014-06-11 10:43:03 -0400

    Clean variables + documentation

--- markroi.m
+++ markroi.m
@@ -1,15 +1,15 @@
 function markroi(varargin)
 % MARKROI Draw outline of ROI onto brain image file.
-%   markedImage = MARKROI(image_dir, Filename, roi_start1, roi_side_len1...roi_startN, roi_side_lenN)
+%   markedImage = MARKROI(imageDirectory, Filename, roiStartArray)
 
-imgDir = varargin{1};
-oldDir = pwd;
-cd(imgDir)
+imageDirectory = varargin{1};
+oldDirectory = pwd;
+cd(imageDirectory)
 
 Filename = varargin{2};
 filename = joinfilename(Filename.prefix, Filename.changing_region, Filename.suffix);
-img_path = strcat(imgDir, '\', filename);
-im = imread(img_path);
+imgagePath = strcat(imageDirectory, '\', filename);
+im = imread(imgagePath);
 imshow(im, [])
 axis off
 hold on
@@ -18,10 +18,12 @@ roiSideLength = varargin{3};
 for i = 1:numel(varargin{4}) %i = number of roiStart in roiStartArray
     rectangle('Position', [varargin{4}{i}(1), varargin{4}{i}(2), roiSideLength, roiSideLength], 'EdgeColor', 'g') %rectangle('Position', [x, y, w, h])
 end
-    
+
+%{
 f = getframe(gca);
 [X, map] = frame2im(f);
-
-%imwrite(X, 'test.tiff');
-cd(oldDir)
+imwrite(X, 'test.tiff');
+%}
+hold off
+cd(oldDirectory)
 end
