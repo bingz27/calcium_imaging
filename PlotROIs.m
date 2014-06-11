c645da997290012ca81edb59ec2c8ddbfd19cdd5
bingz27 <bingz27@gmail.com>
2014-06-11 10:28:13 -0400

    Streamline ability to set multiple ROIs

--- PlotROIs.m
+++ PlotROIs.m
@@ -6,30 +6,34 @@ roiStartArray = {};
 for i = 1:3
     roiStartArray{i} = [740+10*(i-1), 590];
 end
-for i = 4:5
-    roiStartArray{i} = [740+10*(i-1), 600];
+for i = 4:6
+    roiStartArray{i} = [740+10*(i-4), 605];
 end
 roiSideLength = 30;
 startFrame = 240;
 endFrame = 340;
 BaseFilename = Filename('img_', .000000001,'_Default_000.tif'); %based on camera image naming system
 imageDirectory = 'C:\Users\Loturco\Desktop\Bing - Matlab Analysis\sample data\5msecglutamateprepost_1';
-N = 100; % number of multivariate Gaussian (MVG) samples
-DIM = 2; % MVG dimension
-ICs = 5; % number of independent components (PC) to decompose into
 %%% User Inputs
 
+%extract pixels from tiff movie based on set ROIs
 for i = 1:numel(roiStartArray)
     roiRegionArray{i} = setroi(roiStartArray{i}, roiSideLength);
     roiStored{i} = parseimages(imageDirectory, BaseFilename, roiRegionArray{i}, startFrame, endFrame);
 end
-markroi(imageDirectory, BaseFilename, roiSideLength, roiStored{1}, roiStored{2}, roiStored{3},roiStored{4}, roiStored{5});
 
+%mark location of ROIs on the first frame of the movie
+fprintf('Displaying ROI Locations...\n')
+markroi(imageDirectory, BaseFilename, roiSideLength, roiStartArray);
+
+%plot the fluorescence levels over time for each ROI
 figure
 fprintf('Plotting Data...\n')
 time = startFrame:endFrame;
-%plot(time, storedActive)
-numel(time)
-numel(roiStored{1})
-plot(time, roiStored{1}, time, roiStored{2}, time, roiStored{3}, time, roiStored{4}, time, roiStored{5})
+for i = 1:numel(roiStored)
+    plot(time,roiStored{i})
+    hold all
+end
+hold off
+
 fprintf('Done!\n')
--- markroi.m
+++ markroi.m
@@ -15,13 +15,13 @@ axis off
 hold on
 
 roiSideLength = varargin{3};
-for i = 4:nargin
-    rectangle('Position', [varargin{i}(1), varargin{i}(2), roiSideLength, roiSideLength]) %rectangle('Position', [x, y, w, h])
+for i = 1:numel(varargin{4}) %i = number of roiStart in roiStartArray
+    rectangle('Position', [varargin{4}{i}(1), varargin{4}{i}(2), roiSideLength, roiSideLength], 'EdgeColor', 'g') %rectangle('Position', [x, y, w, h])
 end
-
+    
 f = getframe(gca);
 [X, map] = frame2im(f);
 
-imwrite(X, 'test.tiff');
+%imwrite(X, 'test.tiff');
 cd(oldDir)
 end
